54daf81ef93a909f7f74efc352b48cd849800ec9
yuanpengwu04 <yuanpengwu04@gmail.com>
2025-03-09 15:23:34 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js"
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on ${PORT}`);
+});
